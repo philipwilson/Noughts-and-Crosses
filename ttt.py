98e1a1447ff23259811449afa3a47143e5963850
Philip Wilson <wilsonp@Philips-MacBook-Pro.local>
2016-01-25 23:46:39 +0000

    refactor mostly for pylint

--- ttt.py
+++ ttt.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-"""  Do nothing.
+"""  Board and playing strategy for Noughts & Crosses.
 """
 
 import random
 
 class Cell:
-    """ Cells hold blegga.
+    """ Cells are either empty, or hold a player's mark.  I use objects for easy pass-by-reference,
+        so the same cell can be in multiple vectors (row, column, diagonal) in the game.
     """
 
     def __init__(self, position):
@@ -15,76 +16,94 @@ class Cell:
 
     @property
     def empty(self):
-        """  Do nothing.
+        """  Is this cell empty?
         """
         return True if (self.__owner is None) else False
 
     @property
     def player(self):
-        """  Do nothing.
+        """  Which player has marked this cell.
         """
         return "empty" if (self.empty) else self.__owner
 
     @property
     def index(self):
-        """  Do nothing.
+        """  Which cell am I?
         """
         return self.__index
 
     @player.setter
     def player(self, player):
-        """  Do nothing.
+        """  Used to mark the cell for a player.
         """
         if self.empty:
             self.__owner = player
         else:
-            raise Exception("trying to set occupied cell error!")
+            raise Exception("trying to set cell " + str(self.index) + " occupied by " + self.player)
 
 class Board:
-    """ Noughts and Crosses board.
+    """ Noughts and Crosses board.  Contains 9 cells in a 3x3 grid, with vectors for rows,
+        columns, and diagonals.  A player wins by marking all 3 cells in a vector.
     """
     def __init__(self):
         self.board = [Cell(0), Cell(1), Cell(2), Cell(3), Cell(4), Cell(5),
                       Cell(6), Cell(7), Cell(8)]
         self.__turn = 0
         self.players = ['X', 'O']
+        self.empty_char = ' '
 
     @property
     def turn(self):
-        """  Do nothing.
+        """  How many moves have been made so far.
         """
         return self.__turn
 
     @property
     def player_up(self):
-        """  Do nothing.
+        """  Which player plays next.
         """
         return self.players[self.turn % 2]
 
     @property
     def first_player(self):
-        """  Do nothing.
+        """  First player.
         """
         return self.players[0]
 
+    @property
+    def second_player(self):
+        """  Second player.
+        """
+        return self.players[1]
+
     def rows(self):
+        """  Row vectors.
+        """
         return [[self.board[0], self.board[1], self.board[2]],
                 [self.board[3], self.board[4], self.board[5]],
                 [self.board[6], self.board[7], self.board[8]]]
 
     def columns(self):
+        """  Column vectors.
+        """
         return [[self.board[0], self.board[3], self.board[6]],
                 [self.board[1], self.board[4], self.board[7]],
                 [self.board[2], self.board[5], self.board[8]]]
 
     def diagonals(self):
+        """  Diagonal vectors.
+        """
         return [[self.board[0], self.board[4], self.board[8]],
                 [self.board[2], self.board[4], self.board[6]]]
 
     def vectors(self):
+        """  All vectors.
+        """
         return self.rows() + self.columns() + self.diagonals()
 
     def vectors_for_cell(self, cell):
+        """  All the vectors containing this cell.  Lazy inefficient implementation.
+        """
         return [vector for vector in self.vectors() if cell in vector]
 
     def empty_cells(self):
@@ -92,7 +111,7 @@ class Board:
         """
         return [cell for cell in self.board if cell.empty]
 
-    def vector_stats(self, vector):
+    def __vector_stats(self, vector):
         """ Return a dict with cell counts for each player and empty.
         """
         stats = {'empty' : 0}
@@ -107,7 +126,7 @@ class Board:
         """ Does this cell make three in a row?
         """
         for vector in self.vectors_for_cell(cell):
-            stats = self.vector_stats(vector)
+            stats = self.__vector_stats(vector)
             if stats[cell.player] == 3:
                 return True
 
@@ -139,7 +158,7 @@ class Board:
     def __display_cell(self, cell):
         """ Return displayable character for cell's owner (blank char for None.
         """
-        return ' ' if cell.empty else cell.player
+        return self.empty_char if cell.empty else cell.player
 
     def string(self):
         """ Return displayable string representation of the board.
@@ -155,7 +174,7 @@ class Board:
     def winnable_vector(self, vector, player):
         """ Could player win this vector by taking the remaining cell?
         """
-        stats = self.vector_stats(vector)
+        stats = self.__vector_stats(vector)
         return True if (stats[player] == 2 and stats['empty'] == 1) else False
 
     def winning_move_for(self, player):
@@ -166,7 +185,7 @@ class Board:
                 for cell in vector:
                     if cell.empty:
                         return cell.index
-        return False
+        return None
 
     def move_makes_fork(self, cell, player):
         """ A fork is when two different vectors could be won in the next move,
@@ -174,7 +193,7 @@ class Board:
         """
         count = 0
         for vector in self.vectors_for_cell(cell):
-            stats = self.vector_stats(vector)
+            stats = self.__vector_stats(vector)
             if (stats[player] == 1) and (stats['empty'] == 2):
                 count = count + 1
 
@@ -187,100 +206,103 @@ class Board:
         possibles = {}
         for cell in self.empty_cells():
             for vector in self.vectors_for_cell(cell):
-                stats = self.vector_stats(vector)
+                stats = self.__vector_stats(vector)
                 if stats[self.opponent(player)] == 0:
                     possibles[cell.index] = 1
                     break
         return possibles.keys()
 
-# ------------------------------
-# Helper functions:
-# ------------------------------
-
-def debug(msg):
-    print(msg)
 
 def random_from_array(array):
+    """  Return a random element from the array.
+    """
     return array[random.randrange(len(array))]
 
-def play(board, player):
-    """ Strategy for next move.
+def first_move(board, player):
+    """  Grab a corner if moving first.  Randomize for fun..
+    """
+    return random_from_array([0, 2, 6, 8]) if board.turn == 0 else None
+
+def my_winning_move(board, player):
+    """  If player can complete a vector, do it.
+    """
+    return board.winning_move_for(player)
+
+def block_winning_move(board, player):
+    """  If opponent could win next turn, block it.
     """
     opponent = board.opponent(player)
-    empty_cells = board.empty_cells()
+    return board.winning_move_for(opponent)
 
-    # If I have the first move, choose a random corner
-    if board.turn == 0:
-        return board.set_cell(random_from_array([0, 2, 6, 8]), player)
-
-    # Does player have a winning move?  Take it!
-    move = board.winning_move_for(player)
-    if move != False:
-        return board.set_cell(move, player)
-
-    # Does opponent have a winning move?  Block it!
-    move = board.winning_move_for(opponent)
-    if move != False:
-        debug("block win!")
-        return board.set_cell(move, player)
-
-    # Can player create a "fork"?  Do it!
-    for cell in empty_cells:
+def create_fork(board, player):
+    """  Create a fork.  See:  Board.move_makes_fork().
+    """
+    for cell in board.empty_cells():
         if board.move_makes_fork(cell, player):
-            debug("create fork!")
-            return board.set_cell(cell.index, player)
+            return cell.index
+    return None
 
-    # Opponent have diagonal corners with me in center? block a fork and grab a side!
-    if len(empty_cells) == 6:
+def block_diag_fork(board, player):
+    """  Block opposite-corners special case.
+    """
+    if len(board.empty_cells()) == 6:
+        opponent = board.opponent(player)
         for diag in board.diagonals():
             if diag[1].player == player:
                 if diag[0].player == opponent and diag[2].player == opponent:
-                    debug("block opposite-corners fork!")
-                    return board.set_cell(random_from_array([1, 3, 5, 7]), player)
+                    return random_from_array([1, 3, 5, 7])
+    return None
 
-    # Can opponent create a "fork"?  Block it!
-    for cell in empty_cells:
+def block_fork(board, player):
+    """  Stop opponent creating fork.
+    """
+    opponent = board.opponent(player)
+    for cell in board.empty_cells():
         if board.move_makes_fork(cell, opponent):
-            debug("block fork!")
-            return board.set_cell(cell.index, player)
+            return cell.index
+    return None
 
-    # prefer center, then corner, then side
+def default_move(board, player):
+    """  Prefer center, then corners, then sides.
+         First try  moves that could lead to winning!
+    """
     preferred_move_order = [4, 0, 2, 6, 8, 1, 3, 5, 7]
-
-    # prefer moves that could lead to winning!
     could_wins = board.possible_wins(player)
     for move in preferred_move_order:
         if move in could_wins:
-            return board.set_cell(move, player)
+            return move
 
     for move in preferred_move_order:
         if board.cell_empty(move):
-            debug("killing time...")
-            return board.set_cell(move, player)
+            return move
 
-# --------------------
-# Main game loop:
-# --------------------
+def play(board, player):
+    """  Try move generators in order till one returns a move.
+    """
+
+    for func in [first_move, my_winning_move, block_winning_move, create_fork,
+                 block_diag_fork, block_fork, default_move]:
+
+        move = func(board, player)
+        if move is not None:
+            return move
 
 def main():
     """ Main playing loop.  Alternate input between players.
     """
-    tboard = Board()
+    game = Board()
 
-    while len(tboard.empty_cells()) > 0:
-        activeplayer = tboard.player_up
-        if activeplayer == tboard.first_player:
-            #        tcell = play(tboard, activeplayer)
-            umove = int(input("move? "))
-            tcell = tboard.set_cell(umove, activeplayer)
+    while game.turn < 9:
+        if game.player_up == game.first_player:
+            move = int(input("move? "))
         else:
-            tcell = play(tboard, activeplayer)
-            #        umove = int(input("move? "))
-            #        tcell = tboard.set_cell(umove, activeplayer)
-
-        print(tboard.string() + "\n")
-        if tboard.winning_move(tcell):
-            print(tcell.player + " Wins!")
+            move = play(game, game.second_player)
+            
+        cell = game.set_cell(move, game.player_up)
+        
+        print(game.string() + "\n")
+        if game.winning_move(cell):
+            print(cell.player + " Wins!")
             break
 
 if __name__ == "__main__":
